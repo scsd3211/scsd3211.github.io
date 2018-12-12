f4c15d5d96c6939d6e82f78ecf02e1fba270dfe6
scsd3211 <binghunxuluo@qq.com>
2018-12-12 16:35:17 +0800

    chen

---
+++ 2048_files/testjavaScript-haa/haha.js
@@ -0,0 +1,10 @@
+<script>
+/*
+
+haha
+*/
+document.getElementById.("myH1").innerHTML="askjdhakjsdh";
+document.getElementById.("myp").innerHTML = "asdhkajsdasd";
+
+
+</script>
---
+++ 2048_files/testjavaScript-haa/heheda.js
@@ -0,0 +1,7 @@
+<script>
+
+document.getElementById("myH1").innerHTML = "你是个啥";
+document.getElementById("myp").innerHTML = "haha";
+
+
+</script>
